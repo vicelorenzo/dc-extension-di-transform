fbe58a426f17a1cd0ed2b17eba00d8e7de11e2de
eledison <eledison@CLBAOCISDLHD74F>
2022-07-27 22:44:20 +0800

    change parsing condition

--- src/app/editor/di-field.service.ts
+++ src/app/editor/di-field.service.ts
@@ -63,21 +63,26 @@ export class DiFieldService {
   }
 
   parseData() {
-    if (this.data == null) {
-      // initialize the data with an empty object
-      this.data = {
-        crop: [0, 0, 0, 0],
-        rot: 0,
-        hue: 0,
-        sat: 0,
-        bri: 0,
-        fliph: false,
-        flipv: false,
-        poi: {x: -1, y: -1},
-        aspectLock: 'clear',
-        query: ''
-      };
+    if (this.data.image != null) {
+      if (this.data == null) {
+        // initialize the data with an empty object
+        this.data = {
+          crop: [0, 0, 0, 0],
+          rot: 0,
+          hue: 0,
+          sat: 0,
+          bri: 0,
+          fliph: false,
+          flipv: false,
+          poi: {x: -1, y: -1},
+          aspectLock: 'clear',
+          query: ''
+        };
+      }
+    } else {
+      this.data = null
     }
+
     if (this.data.image != null && this.data.image._meta == null) {
       // image is uninitialized.
       this.data.image = null;
